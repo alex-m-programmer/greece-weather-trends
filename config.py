ba680bc22f120f21141eb048f30ec83439563e7f
alex-m-programmer <alexmallis.work@gmail.com>
2026-09-18 05:20:30 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,14 @@
 import os
-import streamlit as st
 from dotenv import load_dotenv
 
 load_dotenv()
 
-try:
-    DB_URL = st.secrets["DB_URL"]
-except (KeyError, FileNotFoundError):
-    DB_URL = os.getenv("DB_URL")
+DB_URL = os.getenv("DB_URL")
 
 if not DB_URL:
     raise ValueError("CRITICAL ERROR: DATABASE_URL not found in environment variables!")
-try:
-    API_KEY = st.secrets["WEATHER_API_KEY"]
-except (KeyError, FileNotFoundError):
-    API_KEY = os.getenv("WEATHER_API_KEY")
+
+API_KEY = os.getenv("WEATHER_API_KEY")
 
 if not API_KEY:
     raise ValueError("CRITICAL ERROR: WEATHER_API_KEY not found in environment variables!")
